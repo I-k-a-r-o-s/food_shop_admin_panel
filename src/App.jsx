263fa82c159510e2ae8d1fc8c3fd4b51059c877f
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-03-25 17:42:56 +0530

    added routes to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
+import { Toaster } from "react-hot-toast";
 import Nabvar from "./components/Nabvar";
 import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router";
+import AddItems from "./pages/AddItems";
+import ListItems from "./pages/ListItems";
+import Orders from "./pages/Orders";
 
 const App = () => {
   return (
-    <div className="drawer lg:drawer-open">
-      <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content">
-        {/* Navbar */}
-        <Nabvar />
-        {/* Page content here */}
-        <div className="p-4">Page Content</div>
+    <div>
+      <Toaster position="bottom-center" reverseOrder={false} />
+      <div className="drawer lg:drawer-open">
+        <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+          {/* Navbar */}
+          <Nabvar />
+          {/* Page content here */}
+          <div className="p-4">
+            <Routes>
+              <Route path="/add" element={<AddItems />} />
+              <Route path="/list" element={<ListItems />} />
+              <Route path="/orders" element={<Orders />} />
+            </Routes>
+          </div>
+        </div>
+        <Sidebar />
       </div>
-      <Sidebar />
     </div>
   );
 };
